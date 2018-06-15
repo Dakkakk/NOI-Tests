749974dd61a7bde20852c5b7d4b6ca60bf0ea98e
jackzhang959 <1049748606@qq.com>
2018-06-15 12:05:06 +0800

    06-15
    Signed-off-by: jackzhang959 <1049748606@qq.com>

--- src/张学荣/校园网络/main.cpp
+++ src/张学荣/校园网络/main.cpp
@@ -1,18 +1,26 @@
 #include<algorithm>
 #include<cstdio>
 #include<cmath>
+#define min(x,y) (x<y?x:y)
 using namespace std;
 int n;
 int map[100][100];
 int main(){
 	freopen("in.txt","r",stdin);
 //	freopen("out.txt","w",stdout);
+	memset(map,127/3,sizeof map);
+	
 	scanf("%d",&n);
 	for(int i=1;i<=n;i++){
 		for(int j=1;j<=n;j++){
 			scanf("%d",&map[i][j]);
 		}
 	}
+	for(int i=1;i<=n;i++)
+		for(int j=1;j<=n;j++)
+			for(int k=1;k<=n;k++)
+				map[j][k]=min(map[j][k],map[j][i]+map[i][k]);
+	
 	
 	return 0;
 }
--- src/王贵鹏/skating.cpp
+++ src/王贵鹏/skating.cpp
@@ -1,26 +1,33 @@
 #include<cstdio>
 #define max(a,b) (a>b? a:b)
 using namespace std;
-int n,m,a[110][110],dp[101][101],ans;
-inline int dfs(int x,int y){
-	if(dp[x][y]!=1) return dp[x][y];
-	int b=0;
-    if(x>=1&&y>=1&&x<=m&&y<=n&&a[x][y]>a[x+1][y])    b=max(b,(dfs(x+1,y)+1));
-    if(x>=1&&y>=1&&x<=m&&y<=n&&a[x][y]>a[x][y+1])    b=max(b,(dfs(x,y+1)+1));
-    if(x>=1&&y>=1&&x<=m&&y<=n&&a[x][y]>a[x-1][y])    b=max(b,(dfs(x-1,y)+1));
-    if(x>=1&&y>=1&&x<=m&&y<=n&&a[x][y]>a[x][y-1])    b=max(b,(dfs(x,y-1)+1));
-    dp[x][y]=max(dp[x][y],b); 
-    return dp[x][y];
+int n,m,a[110][110],dp[101][101],ans,move[4][2]={
+{1,0,},{-1,0,},{0,1,},{0,-1,},};
+bool prove(int xx,int yy) {
+	if(xx>=1&&xx<=m&&yy>=1&&yy<=n)
+		return 1;
+	return 0;
 }
-int main(){
-//	freopen("write.in","r",stdin);
+inline int dfs(int x,int y) {
+	if(dp[x][y]!=1) return dp[x][y];
+	for(int i=0; i<4; i++) {
+		int xx=x+move[i][0];
+		int yy=y+move[i][1];
+		if(prove(xx,yy)&&a[x][y]>a[xx][yy])
+		dp[x][y]=max(dfs(xx,yy)+1,dp[x][y]);//xx yy为到的点，xy原来的点，或，xxyy 原来的点，四周到中间的点； 
+	}//后者代码： 在前需定义一个 aa=1，后再将xy周围点的值算出，遍历；
+	//int aa;……< aa=max(a,dfs(xx,yy)+1); 
+	return dp[x][y];
+}
+
+int main() {
+	freopen("write.in","r",stdin);
 	scanf("%d%d",&m,&n);//m多少行，n多少列；
-	for(int i=1;i<=m;i++)
-		for(int j=1;j<=n;j++)
-		scanf("%d",&a[i][j]),dp[i][j]=1;//输入表格	
-	for(int i=1;i<=m;i++)
-		for(int j=1;j<=n;j++)
-		ans=max(ans,dfs(i,j)); //记忆化搜索
+	for(int i=1; i<=m; i++)
+		for(int j=1; j<=n; j++)
+			scanf("%d",&a[i][j]),dp[i][j]=1;//输入表格
+	for(int i=1; i<=m; i++)
+		for(int j=1; j<=n; j++)
+			ans=max(ans,dfs(i,j)); //记忆化搜索
 	printf("%d",ans);
-	
 }
