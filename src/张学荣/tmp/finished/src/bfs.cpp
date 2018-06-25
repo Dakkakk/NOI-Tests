4ec03fddd591310a263a61f85948ae9b204caaa3
jackzhang959 <1049748606@qq.com>
2018-06-25 12:48:23 +0800

    Signed-off-by: jackzhang959 <1049748606@qq.com>

---
+++ src/张学荣/tmp/finished/src/bfs.cpp
@@ -0,0 +1,15 @@
+#include<cstdio>
+#include<queue>
+//#include<>
+queue<int> q;
+using namespace std;
+int main(){
+	放入第一个点;
+	while(!q.empty()){
+		int i=q.back();
+		if(i的周围有满足条件的点)该点入队; 
+	} 
+	return 0;
+	
+}
+
